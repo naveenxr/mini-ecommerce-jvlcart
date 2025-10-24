79eb9a3a320c70ff6563e86c08012eb3b69476d1
naveenxr <naveennirmala382006@gmail.com>
2025-10-24 15:54:34 +0530

    first version of Mini ecommerse

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,9 +5,10 @@ const productModel = require('../models/productModel');
 exports.createOrder = async (req, res, next) => {
     const cartItems = req.body;
     const amount = Number(cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0)).toFixed(2);
+    
     const status = 'pending';
     const order = await orderModel.create({cartItems, amount, status})
-
+    
     // Updating product stock
     cartItems.forEach(async (item)=> {
         const product = await productModel.findById(item.product._id);
